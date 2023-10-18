2f279e98fb57baf87cb88fb8691daabcacc30a43
Sharad Kadam <145180237+sharadpkadam@users.noreply.github.com>
2023-10-18 18:36:07 +0530

    Add files via upload

---
+++ src/pages/index.jsx
@@ -0,0 +1,14 @@
+export { default as Ecommerce } from './Ecommerce';
+export { default as Help } from './Help';
+export { default as Income } from './Income';
+export { default as Customers } from './Customers';
+export { default as Promote } from './Promote';
+export { default as Product } from './Product';
+export { default as Area } from './Charts/Area';
+export { default as Bar } from './Charts/Bar';
+export { default as ColorMapping } from './Charts/ColorMapping';
+export { default as Financial } from './Charts/Financial';
+export { default as Line } from './Charts/Line';
+export { default as Pie } from './Charts/Pie';
+export { default as Pyramid } from './Charts/Pyramid';
+export { default as Stacked } from './Charts/Stacked';
